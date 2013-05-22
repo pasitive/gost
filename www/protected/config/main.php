e067dcd88dbd72e309bb7cce44184b8e947fd9be
Denis Boldinov <positivejob@yandex.ru>
2013-05-22 13:46:04 +0400

    -удалил ненужное из репозитория, обновил .gitignore

--- www/protected/config/main.php
+++ www/protected/config/main.php
@@ -46,7 +46,7 @@ return array(
 		*/
 
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=gost',
+			'connectionString' => 'mysql:host=127.0.0.1;dbname=gost',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
